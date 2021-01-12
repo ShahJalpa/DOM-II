c32c5da696a3b4759559e537f1f1166576343b62
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-01-12 17:45:32 -0500

    two event are done so far

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+//event Type - Mouse Over - 
+//when cursor goes on any of the nav list it will change the color of nav text
+//and when cursor goes away it set back to the black.
+let navLink = document.querySelectorAll('.nav-link');
+navLink.forEach(item => {
+    item.addEventListener('mouseover', () => {
+       item.style.color = 'red';
+    })
+ })
+
+ navLink.forEach(item => {
+     item.addEventListener('mouseout', () => {
+         item.style.color = 'black';
+     })
+ })
+
+ //make event little larger when mouse clicked twice in a short span of time
+ let img = document.querySelectorAll('.img-content');
+ img.forEach(item => {
+     item.addEventListener('dblclick', () =>{
+         item.classList.toggle('large');
+     })
+ })
+
+
+
+ 
